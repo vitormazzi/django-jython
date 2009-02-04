ccda9b0ca0af0b4ec3263ee824772fd31bf77560
leo.soto <leo.soto@fc56e334-f652-0410-822d-0b1548283ca8>
2009-02-04 01:28:50 +0000

    Removing workaround for jython bug #1245, now that it is solved upstream
    git-svn-id: http://django-jython.googlecode.com/svn/branches/modjy-0.25-integration@51 fc56e334-f652-0410-822d-0b1548283ca8

--- doj/management/commands/war_skel/application.py
+++ doj/management/commands/war_skel/application.py
@@ -3,13 +3,5 @@ import os
 
 def handler(environ, start_response):
     os.putenv("DJANGO_SETTINGS_MODULE", "{{ settings.SETTINGS_MODULE }}")
-    workaround_modjy_passing_unicode_values(environ)
     h = wsgi.WSGIHandler()
     return h(environ, start_response)
-
-# Workaround for jython bug #1245
-def workaround_modjy_passing_unicode_values(environ):
-    for key in environ:
-        if key.startswith('HTTP_') and \
-               isinstance(environ[key], unicode):
-            environ[key] = environ[key].encode('iso-8859-1')
