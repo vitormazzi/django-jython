78825fdba177cd8747d78ca0e0eda07ce87beefe
leo.soto <leo.soto@fc56e334-f652-0410-822d-0b1548283ca8>
2009-12-28 03:28:20 +0000

    Refactored JNDI code to support all backends.
    In order to keep it independent of any particular application server I had to remove the Glassfish-specific code. I want to "undelete" the associated documentation as a Glassfish-specific recipe, but first I need to actually understand it.
    
    Also added a simple tomcat management command which by now only helps with the JNDI configuration but should grow more features on the near future (like deployment, reloading, etc.). It also served to drive the refactoring of the common zxJDBC connection code to the new zxJDBCDatabaseWrapper base class.
    
    git-svn-id: http://django-jython.googlecode.com/svn/branches/jndi@163 fc56e334-f652-0410-822d-0b1548283ca8

--- doj/backends/zxjdbc/postgresql/base.py
+++ doj/backends/zxjdbc/postgresql/base.py
@@ -7,20 +7,16 @@ except ImportError, e:
     from django.core.exceptions import ImproperlyConfigured
     raise ImproperlyConfigured("Error loading zxJDBC module: %s" % e)
 
-try:
-    from javax.naming import Context
-except ImportError, e:
-    from django.core.exceptions import ImproperlyConfigured
-    raise ImproperlyConfigured("Error loading JNDI module: %s" % e)
-
-from django.db.backends import BaseDatabaseWrapper, BaseDatabaseFeatures, BaseDatabaseValidation
+from django.db.backends import BaseDatabaseFeatures, BaseDatabaseValidation
 from django.db.backends.postgresql.operations import DatabaseOperations as PostgresqlDatabaseOperations
 from django.db.backends.postgresql.client import DatabaseClient
 from django.db.backends.postgresql.introspection import DatabaseIntrospection
 from doj.backends.zxjdbc.postgresql.creation import DatabaseCreation
 
-from doj.backends.zxjdbc.common import zxJDBCOperationsMixin, zxJDBCFeaturesMixin
-from doj.backends.zxjdbc.common import zxJDBCCursorWrapper, set_default_isolation_level
+from doj.backends.zxjdbc.common import (
+    zxJDBCDatabaseWrapper, zxJDBCOperationsMixin, zxJDBCFeaturesMixin, 
+    zxJDBCCursorWrapper, set_default_isolation_level)
+
 from com.ziclix.python.sql.handler import PostgresqlDataHandler
 from UserDict import DictMixin
 
@@ -46,7 +42,10 @@ class SettingsModuleAsDict(DictMixin):
     def keys(self):
         return dir(self.module)
 
-class DatabaseWrapper(BaseDatabaseWrapper):
+class DatabaseWrapper(zxJDBCDatabaseWrapper):
+    driver_class_name = "org.postgresql.Driver"
+    jdbc_url_pattern = \
+        "jdbc:postgresql://%(DATABASE_HOST)s%(DATABASE_PORT)s/%(DATABASE_NAME)s"
     operators = {
         'exact': '= %s',
         'iexact': 'ILIKE %s',
@@ -75,36 +74,8 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         self.validation = BaseDatabaseValidation()
 
     def _cursor(self):
-        settings_dict = self.settings_dict
         if self.connection is None:
-            if settings_dict['DATABASE_NAME'] == '':
-                from django.core.exceptions import ImproperlyConfigured
-                raise ImproperlyConfigured("You need to specify DATABASE_NAME in your Django settings file.")
-            host = settings_dict['DATABASE_HOST'] or 'localhost'
-            port = (settings_dict['DATABASE_PORT'] 
-                    and (':%s' % settings_dict['DATABASE_PORT'])
-                    or '')
-            conn_string = "jdbc:postgresql://%s%s/%s" % (
-                    host, port, settings_dict['DATABASE_NAME'])
-            if 'JNDI_NAME' in settings_dict['DATABASE_OPTIONS']:
-                # special case for JNDI lookups
-
-                jndi_name = settings_dict['DATABASE_OPTIONS']['JNDI_NAME']
-                # Default the JNDI endpoint to a Glassfish instance
-                # running on localhost
-                jndi_endpoint = settings_dict['DATABASE_OPTIONS'].get('JNDI_ENDPOINT', 'localhost:3700')
-                jndi_ctx_factory = settings_dict['DATABASE_OPTIONS'].get('JNDI_INITIAL_CONTEXT_FACTORY', 'localhost:3700')
-                props = {'com.sun.appserv.iiop.endpoints':jndi_endpoint,
-                        Context.INITIAL_CONTEXT_FACTORY:jndi_ctx_factory}
-
-                self.connection = Database.lookup(jndi_name, keywords=props)
-            else:
-                self.connection = Database.connect(
-                        conn_string,
-                        settings_dict['DATABASE_USER'],
-                        settings_dict['DATABASE_PASSWORD'],
-                        'org.postgresql.Driver',
-                        **settings_dict['DATABASE_OPTIONS'])
+            self.connection = self.new_connection()
             # make transactions transparent to all cursors
             set_default_isolation_level(self.connection)
         real_cursor = self.connection.cursor()
